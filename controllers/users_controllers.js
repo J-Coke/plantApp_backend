b0544d016f1dd93a69df50f7a0ca586528ede06a
Holly <hollykgreaves@gmail.com>
2021-12-09 12:01:07 +0000

    adds users controller

--- controllers/users_controllers.js
+++ controllers/users_controllers.js
@@ -1,10 +1,10 @@
 const { insertNewUser } = require("../models/users_model");
 
 exports.postNewUser = (req, res, next) => {
-  const newUser = req.body;
-  insertNewUser(newUser)
-    .then((addedUser) => {
-      res.status(201).send(addedUser);
-    })
-    .catch(next);
+	const newUser = req.body;
+	insertNewUser(newUser)
+		.then((addedUser) => {
+			res.status(201).send(addedUser);
+		})
+		.catch(next);
 };
