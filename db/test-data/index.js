7c261fab3e49daf18e005893886c38f98bf44983
Holly <hollykgreaves@gmail.com>
2021-12-09 11:57:14 +0000

    adds index to test data folder

---
+++ db/test-data/index.js
@@ -0,0 +1,5 @@
+const plantData = require("./plant-data");
+const badgeData = require("./badge-data");
+const userData = require("./user-data");
+
+module.exports = { plantData, badgeData, userData };
