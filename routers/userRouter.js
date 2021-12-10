f941354287f69f47e9cd5c878327645019bc70e7
Holly <hollykgreaves@gmail.com>
2021-12-10 11:57:59 +0000

    adds incomplete add new plant router

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -2,9 +2,10 @@ const userRouter = require("express").Router();
 const {
 	postNewUser,
 	patchNewBadge,
+	patchNewPlant,
 } = require("../controllers/users-controllers");
 
 userRouter.post("/", postNewUser);
 userRouter.patch("/:username/badges", patchNewBadge);
-
+userRouter.patch("/:username/plants", patchNewPlant);
 module.exports = userRouter;
