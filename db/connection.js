ca91cc4bace3146a7e3afef5a5951c60e8e3f377
gldrobinson <gldrobinson@hotmail.co.uk>
2021-12-10 20:05:27 +0000

    adds dbName for remoteUri

--- db/connection.js
+++ db/connection.js
@@ -15,6 +15,7 @@ if (ENV === "development") {
 } else if (ENV === "test") {
   dbName = "plantApp_TEST";
 } else if (ENV === "production") {
+  dbName = "plantApp";
   remoteUri =
     "mongodb+srv://plantApp:F1naL1Pr0j3CT@cluster0.m2mod.mongodb.net/plantApp?retryWrites=true&w=majority";
   client = new MongoClient(remoteUri, {
