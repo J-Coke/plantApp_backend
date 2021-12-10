975b97bca7e9f9f3033ed62ba86063fd958e77cb
Holly <hollykgreaves@gmail.com>
2021-12-10 15:28:36 +0000

    adds endpoints router

--- routers/apiRouter.js
+++ routers/apiRouter.js
@@ -2,9 +2,11 @@ const apiRouter = require("express").Router();
 const userRouter = require("./userRouter");
 const plantRouter = require("./plantRouter");
 const badgeRouter = require("./badgeRouter");
+const getEndpoints = require("../controllers/api-controller");
 
 apiRouter.use("/users", userRouter);
 apiRouter.use("/plants", plantRouter);
 apiRouter.use("/badges", badgeRouter);
+apiRouter.get("/", getEndpoints);
 
 module.exports = apiRouter;
