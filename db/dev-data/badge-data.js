abe0500ad89b3688873724c8c05c72faa0b0a757
Holly <hollykgreaves@gmail.com>
2021-12-13 09:48:27 +0000

    adds badge urls to dev data

---
+++ db/dev-data/badge-data.js
@@ -0,0 +1,25 @@
+const badgeData = [
+	{
+		name: "1 week",
+		img_url: "https://i.postimg.cc/gkBBf7bN/aubergine-badge.png",
+	},
+	{ name: "2 week", img_url: "https://i.postimg.cc/HsQ9tPsr/banana-badge.png" },
+	{
+		name: "30 different plants",
+		img_url: "https://i.postimg.cc/XJGwBsz7/broccoli-badge.png",
+	},
+	{
+		name: "60 different plants",
+		img_url: "https://i.postimg.cc/m2GQhNNB/carrot-badge.png",
+	},
+	{ name: "5 seeds", img_url: "https://i.postimg.cc/Wp0ZRqRK/corn-badge.png" },
+	{
+		name: "10 vegetables",
+		img_url: "https://i.postimg.cc/P55v86Ls/garlic-badge.png",
+	},
+	{ name: "5 nuts", img_url: "https://i.postimg.cc/pXGX3Pd4/grape-badge.png" },
+	{
+		name: "5 grains",
+		img_url: "https://i.postimg.cc/267CGDJH/ice-gem-badge.png",
+	},
+];
